6daede8613af4e614f2b4c7249aa84f94a0ebec0
ausprouty <bob.prouty@powertochange.org.au>
2024-07-31 15:21:03 +1000

    file not found

--- App/API/Materials/DownloadMaterialsUpdateUser.php
+++ App/API/Materials/DownloadMaterialsUpdateUser.php
@@ -1,19 +1,32 @@
 <?php
 use App\Controllers\Data\PostInputController;
+use App\Controllers\Materials\MaterialController;
 
 header('Content-Type: application/json');
 
+// check authorization and sanitize data
 $postInputController = new PostInputController();
 $input = $postInputController->handlePost();
-if ($input['success']) {
-    $data = $input['data'];
-    $file_url = RESOURCE_DIR . $data['file'];
-    echo json_encode(array('success' => true, 'file_url' => $file_url));
+if (!$input['success']) {
+    returnError($input['message']);
+    die;
 }
-else{
+$data = $input['data'];
+// see if this material exists in the database
+$materialController = new MaterialController();
+$materialID = $materialController->getIdByFileName($data['file']);
+if (!$materialID) {
+    returnError('File not found');
+    die;
+}
+$file_url = RESOURCE_DIR . $data['file'];
+writeLog('downloadMaterialsUpdateUser-21', $file_url);
+echo json_encode(array('success' => true, 'file_url' => $file_url));
+
+
+function returnError($message){   
+    writeLog('downloadMaterialsUpdateUser-25', $message);          
     echo json_encode(array(
         'success' => false, 
-        'message'=> $input['message']));
-}
-    // Respond with JSON
-   
+        'message'=> $message));
+}  
--- App/Controllers/Materials/MaterialController.php
+++ App/Controllers/Materials/MaterialController.php
@@ -1,24 +1,30 @@
 <?php
-
 namespace App\Controllers\Materials;
 
 use App\Services\DatabaseService;
 use PDO;
 use Exception;
 
-class MaterialQueryController{
-    public static function getTractsToViewQuery() {
-        $dbConnection = new DatabaseConnectionModel();
-        $query = "SELECT languageName
-                  FROM hl_spirit 
-                  ORDER BY languageName";
-        try {
-            $statement = $dbConnection->executeQuery($query);
-            $data = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch all results as array
-            return $data;
-        } catch (Exception $e) {
-            // Log the error or handle it appropriately
-            error_log("Error: " . $e->getMessage());
-            return null;
-        }
+class MaterialController {
+
+    private $databaseService;
+
+    public function __construct() {
+        $this->databaseService = new DatabaseService();
     }
+    public function getIdByFileName($filename) {
+        $query = "SELECT id
+                  FROM hl_materials 
+                  WHERE filename = :filename
+                  LIMIT 1"; 
+        $params = array(            
+            ':filename' => $filename
+        );
+        $results = $this->databaseService->executeQuery($query, $params);
+        $data =  $results->fetch(PDO::FETCH_ASSOC);
+        writeLog('material controller', $data);
+        return $data;
+    }
+    
+   
+}
